0ef28f48a81810377fdd3d67c44a456a63386cc5
Nibesh Khadka <nibeshkhadka@gmail.com>
2024-05-05 18:32:10 +0300

    Part 3, from 3.15- 3.18, phonebook update, delete and middleware

--- part3/backend_phonebook/index.js
+++ part3/backend_phonebook/index.js
@@ -8,29 +8,31 @@ app.use(cors());
 app.use(experss.static("dist"));
 app.use(experss.json());
 
-app.get("/api/persons", (_, res) =>
-  Contact.find({}).then((contacts) => res.json(contacts))
+app.get("/api/persons", (_, res, next) =>
+  Contact.find({})
+    .then((contacts) => res.json(contacts))
+    .catch((e) => next(e))
 );
 
-app.get("/api/persons/:id", (req, res) => {
-  Contact.findById(req.params.id).then((person) => {
-    if (person) {
-      res.status(202).json(person);
-    } else {
-      res.status(404).send("No person found with the id: " + id);
-    }
-  });
+app.get("/api/persons/:id", (req, res, next) => {
+  Contact.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.status(202).json(person);
+      } else {
+        res.status(404).send("No person found with the id: " + id);
+      }
+    })
+    .catch((e) => next(e));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   Contact.findByIdAndDelete(req.params.id)
     .then((_) => res.status(202).end())
-    .catch((e) =>
-      res.status(404).send("No person found with the id: " + req.params.id)
-    );
+    .catch((e) => next(e));
 });
 
-app.post("/api/persons/", (req, res) => {
+app.post("/api/persons/", (req, res, next) => {
   const body = req.body;
   if (!body.name || !body.number) {
     return res
@@ -42,20 +44,66 @@ app.post("/api/persons/", (req, res) => {
     number: Number(body.number),
   });
 
-  newContact.save().then((savedContact) => res.status(202).json(savedContact));
+  newContact
+    .save()
+    .then((savedContact) => res.status(202).json(savedContact))
+    .catch((e) => next(e));
 });
 
-app.get("/info", (req, res) => {
-  Contact.find({}).then((contacts) =>
-    res
-      .status(200)
-      .end(
-        `Phonebook has info for ${contacts.length} people. \n ${new Date(
-          Date.now()
-        )}`
-      )
-  );
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res
+      .status(400)
+      .json({ message: "Name or Phone number is missing " });
+  }
+  const newContact = {
+    name: body.name,
+    number: Number(body.number),
+  };
+
+  Contact.findByIdAndUpdate(req.params.id, newContact, { new: true })
+    .then((updatedContact) => res.status(202).json(updatedContact))
+    .catch((e) => next(e));
 });
+
+app.get("/info", (req, res, next) => {
+  Contact.find({})
+    .then((contacts) =>
+      res
+        .status(200)
+        .end(
+          `Phonebook has info for ${contacts.length} people. \n ${new Date(
+            Date.now()
+          )}`
+        )
+    )
+    .catch((e) => next(e));
+});
+
+const errorHandler = (err, req, res, next) => {
+  if (err.name === "CastError") {
+    return res.status(400).send({ message: "malformatted id" });
+  }
+  if (err.name === "ValidationError") {
+    return res
+      .status(400)
+      .send({
+        message:
+          "The value provided for the phone number contains unsupported format",
+      });
+  }
+
+  //  else {
+  //   return res
+  //     .status(400)
+  //     .send({ message: "Something went wrong, reload and try again!" });
+  // }
+
+  next(err);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
